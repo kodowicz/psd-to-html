ebfa951a250d5155e07dfbae5467196d0e494379
avenria <annakodowicz@gmail.com>
2018-01-27 23:57:53 +0100

    added behavior for video

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-/* slider animation */
+/*   slider animation   */
 let currentSlide = 0;
 
 function changeSlide () {
   const slides = document.querySelectorAll('.slider-item');
   const slidePosition = document.querySelectorAll('.position-item');
+
   slides[currentSlide].className = 'slider-item';
   slidePosition[currentSlide].className = 'position-item';
   currentSlide = (currentSlide+1)%slides.length;
@@ -12,3 +13,25 @@ function changeSlide () {
 }
 
 setInterval(changeSlide, 3000);
+
+
+/*========== playing video ==========*/
+
+const videoButton = document.querySelector('.video-button__button');
+const video = document.querySelector('.promoting-video');
+const videoButtonContainer = document.querySelector('.video-button');
+
+videoButton.addEventListener('click', () => {
+  video.classList.add('promoting-video--video-played');
+  videoButtonContainer.classList.add('video-button--video-played');
+  video.play();
+})
+
+video.addEventListener('click', () => {
+  video.pause();
+  videoButtonContainer.classList.remove('video-button--video-played');
+})
+// if video ends playing
+video.addEventListener('ended', () => {
+  videoButtonContainer.style.display = '';
+})
