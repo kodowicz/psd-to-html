8c675d689eac907a9145f205376d21127a5fc3f0
kodowicz <annakodowicz@gmail.com>
2020-02-23 17:38:30 +0100

    improved navigation

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-/*   slider animation   */
+/* navigation */
+const navigation = document.querySelector('.navigation');
+let lastScrollY = 0;
+
+window.addEventListener('scroll', function(event) {
+  const documentScroll = document.body.getBoundingClientRect().y;
+  const navHeight = navigation.clientHeight;
+
+  if (documentScroll > -10) {
+    navigation.classList.remove('navigation--opened');
+  } else if ((documentScroll < -10) && (-documentScroll > 100)) {
+    navigation.classList.add('navigation--opened');
+  }
+
+  if (lastScrollY > documentScroll) {
+    navigation.style.position = 'fixed';
+    navigation.style.transform = `translateY(-${navHeight}px)`;
+  } else {
+    navigation.style.position = 'fixed';
+    navigation.style.transform = 'translateY(0px)';
+  }
+
+  lastScrollY = documentScroll;
+});
+
+/* slider animation */
 let currentSlide = 0;
 
 function changeSlide () {
@@ -15,7 +40,7 @@ function changeSlide () {
 setInterval(changeSlide, 3000);
 
 
-/*========== playing video ==========*/
+/* playing video */
 
 const videoButton = document.querySelector('.video-button__button');
 const video = document.querySelector('.promoting-video');
@@ -35,3 +60,19 @@ video.addEventListener('click', function () {
 video.addEventListener('ended', function () {
   videoButtonContainer.style.display = '';
 })
+
+
+/* load more images */
+const loadMoreButton = document.querySelector('.works__button');
+
+loadMoreButton.addEventListener('click', function () {
+  const wrapper = document.querySelector('.works__projects');
+  const projects = wrapper.querySelectorAll('.works__project--hidden');
+
+  wrapper.classList.add('works__projects--expanded');
+  loadMoreButton.classList.add('works__button--expanded');
+  projects.forEach(function(project) {
+    project.classList.remove('works__project--hidden');
+  })
+
+});
